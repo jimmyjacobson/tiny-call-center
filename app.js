be731fab7e178abc9297c34d84f7752395c713e4
Ben Peters <bencpeters@gmail.com>
2012-10-17 20:06:26 -0700

    express view config to hbs

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , hbs = require('hbs');
 
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'hbs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
